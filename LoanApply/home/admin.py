5a14736020d3758e26a1b7276dd7e66c4ee378e2
JaiRaavan <n190110@rguktn.ac.in>
2025-01-03 23:10:13 +0530

    Delete LoanApply directory to add new updated LoanApply directory

--- LoanApply/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from home.models import UserProfile, FinancialProfile, CibilData, BankData
-
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(FinancialProfile)
-admin.site.register(CibilData)
-admin.site.register(BankData)
